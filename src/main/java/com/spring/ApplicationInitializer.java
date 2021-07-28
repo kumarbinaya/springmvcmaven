cb34154a8106b588c8def38366ffa0389618a254
kumarbinaya <binayab@emids.com>
2021-07-28 11:11:02 +0530

    code for spring mvc maven project

---
+++ src/main/java/com/spring/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // bootstarp dispather servlet
+        AnnotationConfigWebApplicationContext context= new AnnotationConfigWebApplicationContext();
+        context.register(ApplicationConfig.class);
+        ServletRegistration.Dynamic servletRegistration =  servletContext.addServlet("mvc", new DispatcherServlet(context));
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+    }
+}
